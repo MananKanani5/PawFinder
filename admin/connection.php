3a06206f97c4af2c2b7b53c4edf31f56bdc55e58
Manan Kanani <54730771+MananKanani5@users.noreply.github.com>
2024-08-08 12:45:07 +0530

    Update connection.php

--- admin/connection.php
+++ admin/connection.php
@@ -1,15 +1,15 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'pawfinder');
+   define('DB_SERVER', 'host');
+   define('DB_USERNAME', 'username');
+   define('DB_PASSWORD', 'password');
+   define('DB_DATABASE', 'database_name');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
 
 <?php
-        $dbHost = 'localhost';
-        $dbUser = 'root';
-        $dbPass = '';
-        $dbName = 'pawfinder';
+        $dbHost = 'host';
+        $dbUser = 'username';
+        $dbPass = 'password';
+        $dbName = 'database_name';
         $connection = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 ?>
